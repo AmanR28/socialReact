cad08559d700be2ef337ec120ada66bec402e2b7
Aman28temp <aman28temp@gmail.com>
2019-11-11 02:47:47 +0530

    Corrected apply bug

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,15 +4,15 @@ import PropTypes from "prop-types";
 
 import Scream from "../components/scream/Scream";
 import Profile from "../components/profile/Profile";
+import ScreamSkeleton from "../util/ScreamSkeleton";
+
 import { connect } from "react-redux";
 import { getScreams } from "../redux/actions/dataAction";
-import ScreamSkeleton from "../util/ScreamSkeleton";
 
 class home extends Component {
   componentDidMount() {
     this.props.getScreams();
   }
-
   render() {
     const { screams, loading } = this.props.data;
     let recentScreamsMarkup = !loading ? (
@@ -20,9 +20,8 @@ class home extends Component {
     ) : (
       <ScreamSkeleton />
     );
-
     return (
-      <Grid container spacing={10}>
+      <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
           {recentScreamsMarkup}
         </Grid>
@@ -35,7 +34,7 @@ class home extends Component {
 }
 
 home.propTypes = {
-  getScream: PropTypes.func,
+  getScreams: PropTypes.func.isRequired,
   data: PropTypes.object.isRequired
 };
 
